44538cb5a771fcb7a5d80815bae3dc12830d6e68
Filipe Ramos <ik.ben.filipe@gmail.com>
2020-07-24 23:30:33 -0300

    Deployment: 2nd attempt

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const favicon = require('express-favicon')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
-app.use(favicon(__dirname + '/build/favicon.ico'))
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/ping', function (req, res) {
